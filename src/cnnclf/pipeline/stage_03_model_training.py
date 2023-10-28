ea115cbabe55d403455d4b3a330f31806509f1f2
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-10-28 09:50:35 -0700

    model trainer

---
+++ src/cnnclf/pipeline/stage_03_model_training.py
@@ -0,0 +1,27 @@
+from cnnclf import logger
+from cnnclf.config import ConfigurationManager
+from cnnclf.components.model_training import Training
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
