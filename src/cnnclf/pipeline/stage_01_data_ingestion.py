abffbd72b09247f83122a1d7a87c0d7acb313ffd
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-10-28 08:51:19 -0700

    data ingestion

---
+++ src/cnnclf/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,25 @@
+from cnnclf import logger
+from cnnclf.config import ConfigurationManager
+from cnnclf.components.data_ingestion import DataIngestion
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+STAGE_NAME = "Data Ingestion stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
