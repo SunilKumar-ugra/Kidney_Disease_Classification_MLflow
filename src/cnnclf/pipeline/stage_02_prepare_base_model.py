358d1f580030bbe77cdded3a9df65aa3e47894a7
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-10-28 09:14:48 -0700

    prepare base model

---
+++ src/cnnclf/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,26 @@
+from cnnclf import logger
+from cnnclf.config import ConfigurationManager
+from cnnclf.components.prepare_base_model import PrepareBaseModel
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise 
